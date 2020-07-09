af1a647e926818f1bb8ee9da4cf41b5193866c7e
TheNor01 <Aldo>
2020-07-09 17:31:35 +0200

    speech

--- spark/code/testSpeech.py
+++ spark/code/testSpeech.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
-
 import sys
-
+import json
+from json import loads
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
@@ -13,16 +13,44 @@ ssc = StreamingContext(sc, 1)
 
 zkQuorum="127.0.0.1:2181"
 topic = "myTap"
-kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
+
+
+
+def getName():
+    name=rdd.map(lambda (value): json.loads(value)).map(lambda json_object: json_object["name"])
+    namesCollect=name.collect()
+    if not namesCollect:
+        print("No name")
+        return
+
+
+
+
+kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1},)
+kvs.pprint()
+
+kvs.foreachRDD(getName)
+
+#print(type(kvs))
 
 #dati arrivati in ordine
+
 lines = kvs.map(lambda x: x[1])
-lines.pprint()
+lines2=lines.map(lambda x: loads(x.decode('utf-8')))
+
+#Estrapolo il nome
+#lines3 = lines2.map(lambda (value): json.loads(value)).map(lambda json_object:(json_object["name"]))
+#lines3.pprint()
+
+#Estrapolo l'intero messaggio che arriva
+#lines4 = lines2.map(lambda (value): json.loads(value)).map(lambda json_object:(json_object["message"]))
+#lines4.pprint()
 
 
 
-counts = lines.flatMap(lambda line: line.split(",")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a+b)
-counts.pprint()
+
 
 ssc.start()
 ssc.awaitTermination()
+
+# { \"name\":\"ciccio\",\"Message\" : [ {\"key\":\"ciao\"},{\"key\":\"come\"},{\"key\":\"stai\"}]}
