0c04770245c3a0fcb666b348371fdae2b1c0838b
FrancescoPetrosino <francesco-petrosino@outlook.it>
2020-07-15 17:24:51 +0200

    Aggiornato con i comandi fixati per python2.7

--- main.sh
+++ main.sh
@@ -1,12 +1,26 @@
 #Install py requirements
-sudo apt install pip
+echo "Installo le componenti essenziali.."
+
+echo "[1/] Installo pip..\n"
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python2.7 get-pip.py
+rm get-pip.py
+
+echo "[2/] Installo pulseaudio..\n"
 snap install pulseaudio
 
-pip install SpeechRecognition
+echo "[3/] Installo portaudio..\n"
 sudo apt install python-dev
 sudo apt install portaudio19-dev
-pip install PyAudio
-pip install kafka-python
+
+echo "[4/] Installo SpeechRecognition..\n"
+python2.7 -m pip install SpeechRecognition
+
+echo "[5/] Installo PyAudio..\n"
+python2.7 -m pip install PyAudio
+
+echo "[6/] Installo Kafka-Python..\n"
+python2.7 -m pip install kafka-python
 
 
 # Run main.py
